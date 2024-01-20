d29baceabae81df21ad01fe7036e48b6d674cdb1
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-01-20 11:47:21 +0800

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, Mutex, Once};
 
-use jni::{InitArgsBuilder, JavaVM, JNIVersion, JNIEnv};
 use jni::objects::{JClass, JObjectArray, JValueOwned};
+use jni::{InitArgsBuilder, JNIEnv, JNIVersion, JavaVM};
 
 #[cfg(feature = "graph")]
 mod graph;
@@ -11,7 +11,11 @@ pub extern crate jni;
 pub trait HierarchyClass<'local> {
     fn is_interface(&self, env: &mut JNIEnv<'local>) -> jni::errors::Result<bool>;
 
-    fn is_assignable_from(&self, env: &mut JNIEnv<'local>, other: &JClass<'local>) -> jni::errors::Result<bool>;
+    fn is_assignable_from(
+        &self,
+        env: &mut JNIEnv<'local>,
+        other: &JClass<'local>,
+    ) -> jni::errors::Result<bool>;
 
     fn name(&self, env: &mut JNIEnv<'local>) -> jni::errors::Result<String>;
 
@@ -22,12 +26,15 @@ pub trait HierarchyClass<'local> {
 
 impl<'local> HierarchyClass<'local> for JClass<'local> {
     fn is_interface(&self, env: &mut JNIEnv<'local>) -> jni::errors::Result<bool> {
-        env
-            .call_method(self, "isInterface", "()Z", &[])
+        env.call_method(self, "isInterface", "()Z", &[])
             .and_then(JValueOwned::z)
     }
 
-    fn is_assignable_from(&self, env: &mut JNIEnv<'local>, other: &JClass<'local>) -> jni::errors::Result<bool> {
+    fn is_assignable_from(
+        &self,
+        env: &mut JNIEnv<'local>,
+        other: &JClass<'local>,
+    ) -> jni::errors::Result<bool> {
         env.is_assignable_from(self, other)
     }
 
@@ -35,7 +42,11 @@ impl<'local> HierarchyClass<'local> for JClass<'local> {
         let class_name = env
             .call_method(self, "getName", "()Ljava/lang/String;", &[])
             .and_then(JValueOwned::l)?;
-        unsafe { env.get_string_unchecked((&class_name).into()).map(|java_str| java_str.into()) }.map(|name: String| name.replace(".", "/"))
+        unsafe {
+            env.get_string_unchecked((&class_name).into())
+                .map(|java_str| java_str.into())
+        }
+        .map(|name: String| name.replace(".", "/"))
     }
 
     fn super_class(&self, env: &mut JNIEnv<'local>) -> jni::errors::Result<Option<JClass<'local>>> {
@@ -43,7 +54,8 @@ impl<'local> HierarchyClass<'local> for JClass<'local> {
     }
 
     fn interfaces(&self, env: &mut JNIEnv<'local>) -> jni::errors::Result<Vec<JClass<'local>>> {
-        let interfaces_arr: JObjectArray<'local> = env.call_method(self, "getInterfaces", "()[Ljava/lang/Class;", &[])
+        let interfaces_arr: JObjectArray<'local> = env
+            .call_method(self, "getInterfaces", "()[Ljava/lang/Class;", &[])
             .and_then(JValueOwned::l)?
             .into();
         let interfaces_len = env.get_array_length(&interfaces_arr)?;
@@ -85,12 +97,19 @@ fn thread() -> JNIEnv<'static> {
     jvm().attach_current_thread_permanently().unwrap()
 }
 
-pub fn jclass<'local>(env: &mut JNIEnv<'local>, class_name: &str) -> jni::errors::Result<JClass<'local>> {
+pub fn jclass<'local>(
+    env: &mut JNIEnv<'local>,
+    class_name: &str,
+) -> jni::errors::Result<JClass<'local>> {
     env.find_class(class_name)
 }
 
 /// Get common super class' simple name.
-pub fn common_super_class<'local>(env: &mut JNIEnv<'local>, class1: JClass<'local>, class2: JClass<'local>) -> jni::errors::Result<String> {
+pub fn common_super_class<'local>(
+    env: &mut JNIEnv<'local>,
+    class1: JClass<'local>,
+    class2: JClass<'local>,
+) -> jni::errors::Result<String> {
     let mut cls1: JClass = class1;
     let cls2: JClass = class2;
 
@@ -109,7 +128,7 @@ pub fn common_super_class<'local>(env: &mut JNIEnv<'local>, class1: JClass<'loca
     while {
         cls1 = match cls1.super_class(env)? {
             Some(cls) => cls,
-            None => return Ok(String::from(OBJECT_CLASS_PATH))
+            None => return Ok(String::from(OBJECT_CLASS_PATH)),
         };
 
         !cls2.is_assignable_from(env, &cls1)?
@@ -120,14 +139,17 @@ pub fn common_super_class<'local>(env: &mut JNIEnv<'local>, class1: JClass<'loca
 
 #[cfg(test)]
 mod test {
-    use crate::{common_super_class, thread, HierarchyClass, jclass};
+    use crate::{common_super_class, jclass, thread, HierarchyClass};
 
     #[test]
     fn test_number_common() -> jni::errors::Result<()> {
         let mut env = thread();
         let class1 = jclass(&mut env, "java/lang/Integer")?;
         let class2 = jclass(&mut env, "java/lang/Float")?;
-        assert_eq!("java/lang/Number", common_super_class(&mut env, class1, class2)?);
+        assert_eq!(
+            "java/lang/Number",
+            common_super_class(&mut env, class1, class2)?
+        );
 
         Ok(())
     }
@@ -137,9 +159,19 @@ mod test {
         let mut env = thread();
         let class = jclass(&mut env, "java/lang/Integer")?;
         let interfaces = class.interfaces(&mut env)?;
-        let interface_names = interfaces.iter().map(|interface| interface.name(&mut env)).collect::<jni::errors::Result<Vec<_>>>()?;
+        let interface_names = interfaces
+            .iter()
+            .map(|interface| interface.name(&mut env))
+            .collect::<jni::errors::Result<Vec<_>>>()?;
 
-        assert_eq!(vec!["java/lang/Comparable", "java/lang/constant/Constable", "java/lang/constant/ConstantDesc"], interface_names);
+        assert_eq!(
+            vec![
+                "java/lang/Comparable",
+                "java/lang/constant/Constable",
+                "java/lang/constant/ConstantDesc"
+            ],
+            interface_names
+        );
 
         Ok(())
     }
