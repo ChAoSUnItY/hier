b86bfab66e00b8c6117b7c040702f1245a6cd5f1
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-01-22 03:13:05 +0800

    Create Edge while on class hierarchy tree exploration

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,47 @@
 use dot_writer::DotWriter;
-use jni::{descriptors::Desc, objects::{JClass, GlobalRef}, JNIEnv};
+use jni::{
+    descriptors::Desc,
+    objects::{GlobalRef, JClass},
+    JNIEnv,
+};
 
-use crate::{errors::HierResult as Result, class::{jclass_from_instance, HierExt}};
+use crate::{
+    class::{jclass_from_instance, HierExt},
+    errors::HierResult as Result,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum NodeType {
+    Class,
+    Interface,
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    inner: GlobalRef,
+    node_type: NodeType,
+}
+
+impl Node {
+    pub fn new<'local>(glob_ref: GlobalRef, node_type: NodeType) -> Self {
+        Self {
+            inner: glob_ref,
+            node_type,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Edge {
+    from: Node,
+    to: Node,
+}
+
+impl Edge {
+    pub fn new(from: Node, to: Node) -> Self {
+        Self { from, to }
+    }
+}
 
 pub fn generate_class_hierarchy_tree<'local, 'other_local, T>(
     env: &mut JNIEnv<'local>,
@@ -18,42 +58,54 @@ where
         writer.set_pretty_print(false);
         let mut scope = writer.graph();
 
-        let classes = collect_superclasses_and_interfaces(env, class.as_ref())?;
-        let class_names = classes.iter().map(|(class, derived_class)| {
-            Ok((env.class_name(class)?, env.class_name(derived_class)?))
-        }).collect::<Result<Vec<_>>>()?;
+        let edges = collect_edges(env, class.as_ref())?;
 
-        println!("{:?}", class_names);
+        for Edge { from, to } in edges {
+            let from_class_name = env.class_name(&from.inner)?;
+            let to_class_name = env.class_name(&to.inner)?;
 
-        for (to_class, from_class) in class_names {
-            scope.edge(format!("\"{from_class}\""), format!("\"{to_class}\""));
+            scope.edge(
+                format!("\"{from_class_name}\""),
+                format!("\"{to_class_name}\""),
+            );
         }
     }
 
     Ok(String::from_utf8(output).unwrap())
 }
 
-fn collect_superclasses_and_interfaces<'local, 'other_local>(env: &mut JNIEnv<'local>, class: &JClass<'other_local>) -> Result<Vec<(GlobalRef, GlobalRef)>> {
+fn collect_edges<'local, 'other_local>(
+    env: &mut JNIEnv<'local>,
+    class: &JClass<'other_local>,
+) -> Result<Vec<Edge>> {
     let mut classes = vec![];
     let mut class = jclass_from_instance(env, class)?;
 
     loop {
-        let interfaces = env.interfaces(&class)?
+        let class_node = Node::new(class.clone(), NodeType::Class);
+        let interfaces = env
+            .interfaces(&class)?
             .into_iter()
-            .map(|interface_glob_ref| (class.clone(), interface_glob_ref))
+            .map(|interface_glob_ref| {
+                let interface_node = Node::new(interface_glob_ref, NodeType::Interface);
+
+                Edge::new(class_node.clone(), interface_node)
+            })
             .collect::<Vec<_>>();
 
         classes.extend_from_slice(&interfaces);
-        
+
         let superclass = env.lookup_superclass(&class)?;
 
         match superclass {
             Some(superclass) => {
-                classes.push((class.clone(), superclass.clone()));
+                let superclass_node = Node::new(superclass.clone(), NodeType::Class);
+
+                classes.push(Edge::new(class_node, superclass_node));
 
                 class = superclass;
             }
-            None => break
+            None => break,
         }
     }
 
@@ -64,7 +116,7 @@ fn collect_superclasses_and_interfaces<'local, 'other_local>(env: &mut JNIEnv<'l
 mod test {
     use serial_test::serial;
 
-    use crate::{jni_env, class::HierExt, errors::HierResult as Result};
+    use crate::{class::HierExt, errors::HierResult as Result, jni_env};
 
     use super::generate_class_hierarchy_tree;
 
@@ -78,7 +130,7 @@ mod test {
         println!("{:}", graph);
 
         env.free_lookup()?;
-    
+
         Ok(())
     }
 }
